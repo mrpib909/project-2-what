c4a38d15647858236aba81e069a5a163057e0656
mrpib909 <Ryan@rhickey-router.clients.bradley.edu>
2015-09-29 12:13:50 -0500

    its working

--- what.js
+++ what.js
@@ -2,8 +2,8 @@ var textarea = document.getElementById('what');
 var form = document.getElementsByTagName('form')[0];
 textarea.addEventListener('change', function() {
 	form.submit();
-	var what = location.search;
-	what = what.replace('?what=', '');
-	what = what.replace(/\+/g, ' ');
-  textarea.value = what;
 }, false);
+var what = location.search;
+what = what.replace('?what=', '');
+what = what.replace(/\+/g, ' ');
+textarea.value = what;

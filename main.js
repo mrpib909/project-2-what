e20882f36599c4f6e06789d1b5dbdbcebb1dde99
mrpib909 <Ryan@Ryans-MacBook-Pro.local>
2015-10-01 09:56:12 -0500

    Youtube Gen
    This is a single serving website that will pick ten youtube videos
    randomly using the math random function in javascript Enjoy! :)

---
+++ main.js
@@ -0,0 +1,36 @@
+var vid = document.getElementById('vid');
+
+function test(){
+    var x = Math.floor((Math.random() * 10) + 1);
+    if( x == 1 ){
+vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/1di-Q9kKS88" frameborder="0" allowfullscreen></iframe>';
+    }
+    if( x == 2){
+     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/EglOsfErtaE" frameborder="0" allowfullscreen></iframe>';   
+    }
+    if( x== 3){
+     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE" frameborder="0" allowfullscreen></iframe>';   
+    }
+    if( x== 4){
+     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/6gx79m23lZA" frameborder="0" allowfullscreen></iframe>';   
+    }
+    
+    if(x == 5){
+        vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/HFgAPQ43oXk" frameborder="0" allowfullscreen></iframe>';
+    }
+    if(x == 6){
+        vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/4AFuzwmokGI" frameborder="0" allowfullscreen></iframe>';
+    }
+    if(x == 7){
+        vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/lG5cEik2ABY" frameborder="0" allowfullscreen></iframe>';
+    }
+    if(x == 8){
+        vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/D3gAcyIB358" frameborder="0" allowfullscreen></iframe>';
+    }
+    if(x == 9){
+        vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/75bMzTkwUAY" frameborder="0" allowfullscreen></iframe>';
+    }
+    if(x == 10){
+        vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/oNpMDN4QqD8" frameborder="0" allowfullscreen></iframe>';
+    }
+}
--- what.js
+++
@@ -1,9 +0,0 @@
-var textarea = document.getElementById('what');
-var form = document.getElementsByTagName('form')[0];
-textarea.addEventListener('change', function() {
-	form.submit();
-}, false);
-var what = location.search;
-what = what.replace('?what=', '');
-what = what.replace(/\+/g, ' ');
-textarea.value = what;

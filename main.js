5cf6a82cf4d2a3a2725ccab96a988972ecb2c018
mrpib909 <Ryan@Ryans-MacBook-Pro.local>
2015-10-04 21:14:05 -0500

    New java script and html added

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var vid = document.getElementById('vid');
-
+document.getElementById("get").addEventListener("click", test);
 function test(){
     var x = Math.floor((Math.random() * 10) + 1);
     if( x == 1 ){
-vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/1di-Q9kKS88" frameborder="0" allowfullscreen></iframe>';
+vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/DsLRuwPX6c8" frameborder="0" allowfullscreen></iframe>';
     }
     if( x == 2){
-     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/EglOsfErtaE" frameborder="0" allowfullscreen></iframe>';   
+     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/EglOsfErtaE" frameborder="0" allowfullscreen></iframe>';
     }
     if( x== 3){
-     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE" frameborder="0" allowfullscreen></iframe>';   
+     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/jofNR_WkoCE" frameborder="0" allowfullscreen></iframe>';
     }
     if( x== 4){
-     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/6gx79m23lZA" frameborder="0" allowfullscreen></iframe>';   
+     vid.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/6gx79m23lZA" frameborder="0" allowfullscreen></iframe>';
     }
-    
+
     if(x == 5){
         vid.innerHTML ='<iframe width="560" height="315" src="https://www.youtube.com/embed/HFgAPQ43oXk" frameborder="0" allowfullscreen></iframe>';
     }
